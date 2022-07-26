f97b70eacbb6a4d71659aae369fead115cf4eebe
ropoko <rodrigostramantinoli@gmail.com>
2022-07-25 22:48:45 -0300

    feat: added suit for graphic interface

--- main.lua
+++ main.lua
@@ -1,3 +1,18 @@
+local suit = require 'suit'
+
+local show_message = false
+function love.update(dt)
+	-- Put a button on the screen. If hit, show a message.
+	if suit.Button("Hello, World!", 100,100, 300,30).hit then
+		show_message = true
+	end
+
+	-- if the button was pressed at least one time, but a label below
+	if show_message then
+		suit.Label("How are you today?", 100,150, 300,30)
+	end
+end
+
 function love.draw()
-	love.graphics.print("Hello World!", 400, 300)
+	suit.draw()
 end
