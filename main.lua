8dcce843b9915fedcf3fcbd4588efd9aecf1dafa
ropoko <rodrigostramantinoli@gmail.com>
2022-07-27 22:53:59 -0300

    chore: removed age prop from user

--- main.lua
+++ main.lua
@@ -1,35 +1,25 @@
 local Suit = require './lib/suit'
 local User = require './src/entities/user'
+local Constants = require './src/constants'
+local Saver = require('./src/helpers/saver')
 
-local name_input = { text = '' }
-local age_input = { text = '' }
-
-local under_age = false
+local nickname_input = { text = '' }
 
 function love.update(dt)
-	Suit.Label('Name: ', { align='left' }, 10, 10, 200, 30)
-	Suit.Input(name_input, 10, 40, 200, 30)
+	Suit.Label('Nickname: ', { align='left' }, 10, 10, 200, 30)
+	Suit.Input(nickname_input, 10, 40, 200, 30)
 
-	Suit.Label('Age: ', { align='left' }, 10, 70, 200, 30)
-	Suit.Input(age_input, 10, 100, 200, 30)
-
-	if Suit.Button('Send', 10, 150, 200, 30).hit then
-		-- Saver.save()
-
-		if tonumber(age_input.text) < 18 then under_age = true
-		else under_age = true end
-	end
-
-	if under_age then
-		Suit.Label('Must be over 18 years old!', { align='center' }, 10, 200, 200, 30)
+	if Suit.Button('Send', 10, 80, 200, 30).hit then
+		Saver.save()
 	end
 end
 
 function love.load()
-	LOGGED_USER = User('ropoko', 32)
+	local data = Saver:retrieveData()
 
-	-- local Saver = require('./src/helpers/saver')
-	-- Saver.save()
+	if data ~= nil then
+		Constants.LOGGED_USER = data
+	end
 end
 
 function love.draw()
