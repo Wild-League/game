e4afb8a15f411f16561322143ed3d754c4033db3
ropoko <rodrigostramantinoli@gmail.com>
2022-11-01 22:35:45 -0300

    refactor: changed how the state machine works

--- src/context.lua
+++ src/context.lua
@@ -3,25 +3,28 @@ local Get_Info = require('src/states/get_info')
 local In_Game = require('src/states/in_game')
 
 local Context = {
-	-- states
-	initial = Initial,
-	get_info = Get_Info,
-	in_game = In_Game,
+	states = {
+		initial = Initial,
+		get_info = Get_Info,
+		in_game = In_Game
+	},
 
 	set_current = 'initial',
 
 	current = function(self)
-		return self[self.set_current]()
+		return self.states[self.set_current]()
 	end,
 
 	change = function(self, ctx)
-		print('changing states', ctx)
+		print('changing state for: ', ctx)
 
 		if ctx == nil then
 			error('Context should not be nil')
 		end
 
-		if ctx == 'initial' or ctx == 'get_info' or ctx == 'in_game' then
+		local new_ctx = self.states[ctx]
+
+		if new_ctx ~= nil then
 			self.set_current = ctx
 		else
 			error('This context does not exist')
