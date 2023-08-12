5716268727c35e57c5e416b261d5583630d390b2
ropoko <rodrigostramantinoli@gmail.com>
2023-08-12 01:51:47 -0300

    fix: deck now anchors on the screen

--- src/helpers/layout.lua
+++ src/helpers/layout.lua
@@ -9,12 +9,12 @@ function Layout:center(obj_width, obj_height)
 	obj_width = obj_width or 0
 	obj_height = obj_height or 0
 
-	local central = {
+	local center = {
 		width = ((width - obj_width) / 2),
 		height = ((height - obj_height) / 2)
 	}
 
-	return central
+	return center
 end
 
 function Layout:down_left(obj_width, obj_height)
@@ -29,6 +29,21 @@ function Layout:down_left(obj_width, obj_height)
 	}
 
 	return down_left
+end
+
+function Layout:down_right(obj_width, obj_height)
+	local width = Constants.WINDOW_SETTINGS.width
+	local height = Constants.WINDOW_SETTINGS.height
+
+	obj_width = obj_width or 0
+	obj_height = obj_height or 0
+
+	local down_right = {
+		width = width - obj_width,
+		height = height - obj_height
+	}
+
+	return down_right
 end
 
 function Layout:up_right(obj_width, obj_height)
