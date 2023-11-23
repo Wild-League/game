168e70d6c72dda2f3aee71a9e6b47db5bd416c1f
ropoko <rodrigostramantinoli@gmail.com>
2023-11-23 00:15:22 -0300

    fix: change window size

--- conf.lua
+++ conf.lua
@@ -1,10 +1,11 @@
 function love.conf(t)
 	t.window.title = 'Wild League'
 	t.window.resizable = false
-	t.window.width = 1600
-	t.window.height = 900
+	t.window.width = 1344
+	t.window.height = 756
 	t.window.minwidth = 800
 	t.window.minheight = 600
+	t.window.displayindex = 2 -- only for testing
 	t.window.tightdpi = true
   t.window.usedpiscale = true
 end
