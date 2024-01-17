727451f6ce8067bf900525cc9a2e122275818e53
ropoko <rodrigostramantinoli@gmail.com>
2024-01-16 22:40:54 -0300

    feat: improved UI for initial state

--- src/states/initial.lua
+++ src/states/initial.lua
@@ -4,6 +4,8 @@ local Layout = require('src.helpers.layout')
 local Instance = require('src.api.instance')
 
 local Initial = {
+	logo = love.graphics.newImage('assets/logo-text.png'),
+
 	instance_input = { text = 'https://wildleague.org' },
 	instance_invalid = false
 }
@@ -11,20 +13,30 @@ local Initial = {
 function Initial:load() end
 
 function Initial:update()
-	local center = Layout:center(100, 100 - 200)
-	Suit.Input(self.instance_input, center.width, center.height - 80, 300, 30)
+	local input_center = Layout:center(300, 30)
+	Suit.Input(self.instance_input, input_center.width, input_center.height + 100, 300, 30)
 end
 
 function Initial:draw()
-	local center = Layout:center(100, 100 - 200)
+	love.graphics.setBackgroundColor(10/255,16/255,115/255)
 
-	Suit.Label('Enter the instance you want to join', center.width, center.height - 200, 300, 200)
+	local default_scale = 0.3
+	local center_logo = Layout:center(self.logo:getWidth() * default_scale, self.logo:getHeight() * default_scale)
 
-	local play_button = Suit.Button('Enter', center.width, center.height)
+	love.graphics.push()
+	love.graphics.translate(center_logo.width, center_logo.height - 100)
+	love.graphics.scale(default_scale)
+	love.graphics.draw(self.logo, 0, 0)
+	love.graphics.pop()
+
+	local text_center = Layout:center(350, 100)
+	Suit.Label('Enter the server you want to join!', text_center.width, text_center.height, 350, 200)
+
+	local play_button = Suit.Button('Enter', text_center.width + 25, text_center.height + 200, 300, 30)
 
 	if self.instance_invalid then
 		love.graphics.setColor(1,0,0)
-		love.graphics.print('This instance is not valid.', center.width, center.height - 250)
+		love.graphics.print('This instance is not valid.', text_center.width, text_center.height - 250, 250)
 		love.graphics.setColor(0,0,0)
 	end
 
