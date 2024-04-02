8d7142970bf567c024854e9490fd602f0fe44100
ropoko <rodrigostramantinoli@gmail.com>
2024-04-02 10:32:34 -0300

    feat: wip implementing nakama on lobby

--- src/states/lobby.lua
+++ src/states/lobby.lua
@@ -1,28 +1,68 @@
-local User = require('src.api.user')
 local Suit = require('lib.suit')
-
--- TODO: draw the actual lobby
+local Layout = require('src.helpers.layout')
+local nakama = require('lib.nakama.nakama')
+local love2d = require('lib.nakama.engine.love2d')
+local https = require('https')
+local socket = require('socket')
 
 local Lobby = {
-	user = {}
+	server = {}
 }
 
-function Lobby:load()
-	-- self.user = User:get()
-end
+local client = nakama.create_client({
+	host = 'localhost',
+	port = 7351,
+	username = 'admin',
+	password = 'password',
+	engine = love2d
+})
+
+local co = coroutine.create(function()
+	-- authenticate
+	local result = nakama.authenticate_device(client, '11000000-0000-0000-0000-000000000000', nil, true)
+
+	print('result', result)
+
+	if result then
+		nakama.set_bearer_token(client, result.token)
+	end
+
+	Lobby.server = nakama.create_socket(client)
+	print('server', Lobby.server)
+
+	local ok,err = nakama.socket_connect(Lobby.server)
+	print('ok: ', ok, 'err: ', err)
+
+	-- Lobby.server:connect()
+end)
+
+coroutine.resume(co)
+
+-- sock.match_create(sock, 'first match')
+-- local ok,err = nakama.socket_connect(sock)
+
+-- print('ok: ', ok, 'err: ', err)
+
+-- server:connect()
+
+function Lobby:load() end
 
 function Lobby:update() end
 
 function Lobby:draw()
-	local play_button = Suit.Button('Play', 10, 10)
+	love.graphics.setBackgroundColor(10/255,16/255,115/255)
+
+	local center = Layout:center(300, 30)
+
+	local play_button = Suit.Button('Search Match', center.width, center.height, 300, 40)
+
+	Suit.Label('Welcome to our alpha v0.0.1 ', center.width, 100)
+	Suit.Label('For now, you can only play with 3 cards', center.width - 50, 130)
+	Suit.Label('Search for a match or play against a friend', center.width - 70, 160)
 
 	if play_button.hit then
-		CONTEXT:change('queue')
+		self.server.match_create('new match')
 	end
-
-	-- love.graphics.print('Lobby', 10, 10)
-	-- love.graphics.print('Welcome '..self.user.username, 10, 30)
-	-- love.graphics.print('Level: '..self.user.level, 10, 50)
 end
 
 return Lobby
