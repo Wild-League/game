366976dadfdf10641d6510eda10794c20cbe73a0
Felipe Gabriel <felipe.ramiressanches@hotmail.com>
2024-11-07 19:59:08 -0300

    feat: started card area on DeckSelection

--- src/states/deck_selection.lua
+++ src/states/deck_selection.lua
@@ -24,13 +24,39 @@ function DeckSelection:update(dt) end
 
 function DeckSelection:draw()
 
-    love.graphics.setColor(0.1, 0.1, 0.1, 0.8)
-    love.graphics.rectangle('fill', love.graphics.getWidth() / 10,
-                            love.graphics.getHeight() / 1.3,
-                            love.graphics.getWidth() / 1.3,
-                            love.graphics.getHeight())
-    love.graphics.setColor(1, 1, 1, 1)
+    local cards = {0, 1, 2, 3, 4, 5, 6}
+
+    self.card_area(.5, cards)
+    self.card_area(3, cards)
     FriendsSidebar.draw(Suit, Fonts, DeckSelection)
+
+end
+
+function DeckSelection.card_area(y, cards)
+    love.graphics.setColor(0.1, 0.1, 0.1, 0.8)
+
+    local height = love.graphics.getHeight() / 6;
+
+    love.graphics.rectangle('fill', love.graphics.getWidth() / 10, height * y,
+                            love.graphics.getWidth() / 1.5,
+                            love.graphics.getHeight() / 3)
+    love.graphics.setColor(1, 1, 1, 1)
+
+    if cards.length == 0 then return end
+
+    local card_width, card_height = ((love.graphics.getWidth() / 1.5) - 40) / 6,
+                                    (love.graphics.getHeight() / 3) - 40
+
+    for index, card in pairs(cards) do
+        print("index: ", index)
+        print("card: ", card)
+        love.graphics.rectangle('fill',
+                                ((love.graphics.getWidth() / 10) + 20) +
+                                    (20 * index), (height * y) + 20, card_width,
+                                card_height)
+
+        love.graphics.setColor(1, 1, 1, 1)
+    end
 
 end
 
